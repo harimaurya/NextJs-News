d104d3b131f0e3e423896411d21c79832e3f2035
Hari Maurya <4748625+harimaurya@users.noreply.github.com>
2025-08-07 10:41:11 +1200

    Styled heading

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div id="home">
-      <h1>Welcome to Next.js News</h1>
+      <h1 className="mt-[3rem] text-4xl font-bold text-center">Welcome to Next.js News</h1>
     </div>
   );
 }
