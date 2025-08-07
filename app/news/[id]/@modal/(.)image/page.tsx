373a5d3792ebfe26e35aab710f6be62d52d47e49
Hari Maurya <4748625+harimaurya@users.noreply.github.com>
2025-08-07 21:31:06 +1200

    Handle modal close

--- app/news/[id]/@modal/(.)image/page.tsx
+++ app/news/[id]/@modal/(.)image/page.tsx
@@ -1,13 +1,18 @@
+"use client";
+
 import { DUMMY_NEWS } from "@/data/news";
 import Image from "next/image";
-import { notFound } from "next/navigation";
+import { notFound, useRouter } from "next/navigation";
 
-export default async function InterceptedImagePage({
+
+export default function InterceptedImagePage({
   params,
 }: {
   params: { id: string };
 }) {
-  const { id } = await params;
+  const router = useRouter();
+
+  const { id } = params;
   const newsItem = DUMMY_NEWS.find((news) => news.id === id);
 
   if (!newsItem) {
@@ -16,8 +21,14 @@ export default async function InterceptedImagePage({
 
   return (
     <>
-      <div className="modal-backdrop fixed inset-0 bg-black opacity-50"></div>
-      <dialog className="modal-content m-auto fixed inset-0 flex items-center justify-center" open>
+      <div
+        className="modal-backdrop fixed inset-0 bg-black opacity-50"
+        onClick={router.back}
+      ></div>
+      <dialog
+        className="modal-content m-auto fixed inset-0 flex items-center justify-center"
+        open
+      >
         <Image
           src={`/images/news/${newsItem.image}`}
           alt={newsItem.title}
